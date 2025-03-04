365ef12c19586b1cff3992e51ef6028cb75aa4e4
faisalindrianto <wwf051099@gmail.com>
2025-03-04 15:45:29 +0700

    feat: setup @pinia/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/scss/app.scss'],
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', '@vueuse/nuxt'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/icon',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+  ],
 })
